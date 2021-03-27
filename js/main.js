7bb0c349ee5987b4c349a1f41343d538039487e6
mark l chaves <mark@caughtmyeye.cc>
2021-03-27 17:55:35 +0800

    Added art direction hero
    Added footer credits and linked the footer Team to the team section. Added CSS offset hack for anchor scrolling.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-/** 
- * Legacy function. 
- * 
+/**
+ * Legacy function.
+ *
  * Issues: Needs hardcoded offsets and
  * doesn't preserve browser history.
- * 
- * TODO: Try to refactor into JS function
+ *
+ * TO DO: Try to refactor into JS function
  * in index.html that handles hero button
  * anchor.
- * 
+ *
  * mlc
  */
-$(document).ready(function(){
- $('.header').height($(window).height());
+$(document).ready(function () {
+  $(".header").height($(window).height());
 
- $(".navbar a").click(function(){
-	 let hdrOffset = 60;
-	 if ($(window).width() < 1024) hdrOffset = 200;
- 	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top-hdrOffset
- 	},1000)
-  
- })
-
-})
+  $(".navbar a").click(function () {
+    let hdrOffset = 60;
+    if ($(window).width() < 1024) hdrOffset = 200;
+    $("body,html").animate(
+      {
+        scrollTop: $("#" + $(this).data("value")).offset().top - hdrOffset,
+      },
+      1000
+    );
+  });
+});
