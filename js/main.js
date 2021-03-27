59555edc437d97288e9b1076cfd7e676eeacc22c
mark l chaves <mark@caughtmyeye.cc>
2021-03-27 12:10:32 +0800

    Fixes: menu offset, card and contact margin

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+/** 
+ * Legacy function. 
+ * 
+ * Issues: Needs hardcoded offsets and
+ * doesn't preserve browser history.
+ * 
+ * TODO: Try to refactor into JS function
+ * in index.html that handles hero button
+ * anchor.
+ * 
+ * mlc
+ */
 $(document).ready(function(){
  $('.header').height($(window).height());
 
  $(".navbar a").click(function(){
+	 let hdrOffset = 60;
+	 if ($(window).width() < 1024) hdrOffset = 200;
  	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top
+ 		scrollTop:$("#" + $(this).data('value')).offset().top-hdrOffset
  	},1000)
   
  })
